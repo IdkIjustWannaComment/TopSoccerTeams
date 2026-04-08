82679f206cf9eb15d2fbba5ce3f6ad4587ef9a9d
Nikhil Govender <nikhil.govender01@gmail.com>
2026-04-08 09:18:47 +0200

    Made a new teams variable and added display code

--- app/src/main/java/com/example/topsoccerteams/MainActivity.kt
+++ app/src/main/java/com/example/topsoccerteams/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.topsoccerteams
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +12,17 @@ class MainActivity : AppCompatActivity() {
     val teams = arrayOf<String>("Mamelodi Sundowns FC", "Orlando Pirates",
         "Bidvest Wits", "Stellenbosch FC",
         "Sehukune united FC")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val teamsOutput = findViewById<TextView>(R.id.txtOutput)
+
+        teamsOutput.text = teams[0]
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)

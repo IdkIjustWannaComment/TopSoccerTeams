1412bd897b01b10e4014a98b473b1061aa6ffc06
Nikhil Govender <nikhil.govender01@gmail.com>
2026-04-17 15:05:07 +0200

    added and completed longest name function

--- app/src/main/java/com/example/topsoccerteams/MainActivity.kt
+++ app/src/main/java/com/example/topsoccerteams/MainActivity.kt
@@ -9,7 +9,15 @@ import androidx.core.view.WindowInsetsCompat
 import java.util.Arrays
 import android.util.Log
 
-fun getLongestName()
+fun getLongestName(arr: Array<String>): String {
+    var longest = arr[0]
+
+    for (name in arr) {  if (name.length > longest.length) {
+            longest = name
+        }
+    }
+    return longest
+}
 
 fun logArrayValues(arr: Array<String>, limit: Int = 0) {if (limit == 0)
 {Log.v("Array Values", Arrays.toString(arr))}
@@ -41,6 +49,10 @@ class MainActivity : AppCompatActivity() {
 
 
         }
+
+        val longestTeam = getLongestName(teams)
+        teamsDisplay += "\nLongest Team Name:\n$longestTeam"
+        teamsOutput.text = teamsDisplay
 
         logArrayValues(teams)
         logArrayValues(teams,3)
